1cc26b07967795b7b574e0e949476b3029cc046a
Siyu <13501954683@163.com>
2020-03-31 23:24:11 -0400

    Add one more hiden layer -- Siyu

--- DQN/DQN.py
+++ DQN/DQN.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 # Hyper Parameters
-BATCH_SIZE = 64
+BATCH_SIZE = 1024
 LR = 0.01                   # learning rate
 EPSILON = 0.9               # greedy policy
 GAMMA = 0.9                 # reward discount
@@ -15,13 +15,17 @@ MEMORY_CAPACITY = 4000
 class Net(nn.Module):
     def __init__(self, N_STATES, N_ACTIONS):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(N_STATES,20)
+        self.fc1 = nn.Linear(N_STATES,N_STATES // 4)
         self.fc1.weight.data.normal_(0, 0.1)   # initialization
-        self.out = nn.Linear(20, N_ACTIONS)
+        self.fc2 = nn.Linear(N_STATES // 4, N_STATES // 4)
+        self.fc2.weight.data.normal_(0, 0.1)  # initialization
+        self.out = nn.Linear(N_STATES // 4, N_ACTIONS)
         self.out.weight.data.normal_(0, 0.1)   # initialization
 
     def forward(self, x):
         x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
         x = F.relu(x)
         actions_value = self.out(x)
         return actions_value
